0a8333fc5669fd83ac1272814200c231836fa56d
lucas-soria <lucas.soria@mercadolibre.com>
2022-10-24 13:09:16 -0300

    Go Testing - Clase 4 - 2022-10-24 - Testeo de todos los puntos de retorno

--- go-testing/desafio-cierre-testing-main/internal/products/handler_test.go
+++ go-testing/desafio-cierre-testing-main/internal/products/handler_test.go
@@ -3,6 +3,7 @@ package products
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -14,11 +15,14 @@ const (
 	BaseURL = "/api/v1/products"
 )
 
-func createServer() (engine *gin.Engine) {
+func createServer(err error) (engine *gin.Engine) {
 	// Initialization of services
 	productsRepository := NewRepository()
-	productsService := NewService(productsRepository)
-	productsHandler := NewHandler(productsService)
+	productsService := mockService{
+		repository: productsRepository,
+		ErrGet:     err,
+	}
+	productsHandler := NewHandler(&productsService)
 	engine = gin.New()
 	// Endpoints
 	// Products
@@ -38,7 +42,7 @@ func createRequestTest(method string, url string, body string) (*http.Request, *
 }
 
 func testGetProductsOk(t *testing.T) {
-	server := createServer()
+	server := createServer(nil)
 	getSellerID := "?seller_id=FEX112AC"
 	expected := []Product{
 		{
@@ -58,7 +62,7 @@ func testGetProductsOk(t *testing.T) {
 }
 
 func testGetProductsBadSellerID(t *testing.T) {
-	server := createServer()
+	server := createServer(nil)
 	expected := map[string]string{
 		"error": "seller_id query param is required",
 	}
@@ -71,7 +75,28 @@ func testGetProductsBadSellerID(t *testing.T) {
 	assert.Equal(t, expected, responseObject)
 }
 
+func testGetProductsWithErr(t *testing.T) {
+	getSellerID := "?seller_id=FEX112AC"
+	expectedErr := errors.New("forced error in service")
+	server := createServer(expectedErr)
+	expected := map[string]string{
+		"error": expectedErr.Error(),
+	}
+	request, responseRecorder := createRequestTest(http.MethodGet, BaseURL+getSellerID, "")
+	server.ServeHTTP(responseRecorder, request)
+	var responseObject map[string]string
+	err := json.Unmarshal(responseRecorder.Body.Bytes(), &responseObject)
+	assert.Equal(t, http.StatusInternalServerError, responseRecorder.Code)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, responseObject)
+}
+
+func testBadPathSuit(t *testing.T) {
+	t.Run("Bad SellerID", testGetProductsBadSellerID)
+	t.Run("Error on service", testGetProductsWithErr)
+}
+
 func TestSuitGET(t *testing.T) {
 	t.Run("Happy path", testGetProductsOk)
-	t.Run("Sad Path/Bad SellerID", testGetProductsBadSellerID)
+	t.Run("Sad Path", testBadPathSuit)
 }
